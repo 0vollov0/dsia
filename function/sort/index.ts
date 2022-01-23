5cd6f6a6345562fde7966df6d35513d1e66438d7
Park Sin Gyu <40098076+0vollov0@users.noreply.github.com>
2022-01-23 16:00:45 +0900

    pur bubblesort int index

--- function/sort/index.ts
+++ function/sort/index.ts
@@ -1,3 +1,15 @@
-import bubbleSort from "./bubbleSort";
+function bubbleSort(array: number[]) {
+  for (let i = 1; i < array.length; i++) {
+    for (let j = 0; j < array.length-i; j++) {
+    console.log(j);
+
+      if (array[j] > array[j+1]) {
+        const temp = array[j];
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }
+  }
+}
 
 export {bubbleSort}
