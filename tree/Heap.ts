b17cdf635c313ff6700ae7bbd65201e0278d5581
park singyu <psk876@naver.com>
2022-01-25 21:22:24 +0900

    add sort method and fix logic

--- tree/Heap.ts
+++ tree/Heap.ts
@@ -6,11 +6,11 @@ class Heap {
     this.is_max_heap = is_max_heap;
   }
   
-  get tree() {
+  get tree(): number[] {
     return this._tree.slice(1,this._tree.length);
   }
   
-  insert(data: number) {
+  insert(data: number): void {
     this._tree.push(data);
     let new_node_index = this._tree.length-1;
     let parent_index = Math.floor(new_node_index/2);
@@ -27,33 +27,57 @@ class Heap {
     }
   }
 
-  pop() {
+  pop(): number {
     if (this._tree.length <= 1) return;
+    const return_value = this._tree[1];
     this._tree[1] = this._tree[this._tree.length-1];
-    this._tree.length = this._tree.length-1;
+    this._tree.length--;
     
     let parent_node_index = 1;
-    if (this.is_max_heap) {
-      while (true) {
-        const child_node_index = this._tree[parent_node_index*2] > this._tree[parent_node_index*2+1] ? parent_node_index*2 : parent_node_index*2+1;        
-        if ((child_node_index < this._tree.length) && (this._tree[parent_node_index] < this._tree[child_node_index])) {
+    let children_count = 0;
+    let child_node_index = 0;
+    while (true) {
+      children_count = this._tree.length - (parent_node_index*2);
+      if (1 < children_count) {
+        if (this.is_max_heap) {
+          child_node_index = this._tree[parent_node_index*2] > this._tree[parent_node_index*2+1]
+            ? parent_node_index*2
+            : parent_node_index*2+1; 
+        } else {
+          child_node_index = this._tree[parent_node_index*2] < this._tree[parent_node_index*2+1]
+            ? parent_node_index*2
+            : parent_node_index*2+1; 
+        }
+      } else if (children_count === 1) {
+        child_node_index = parent_node_index*2;
+      } else break;
+
+      if (this.is_max_heap) {
+        if (this._tree[parent_node_index] < this._tree[child_node_index]) {
+          const temp = this._tree[parent_node_index];
+          this._tree[parent_node_index] = this._tree[child_node_index];
+          this._tree[child_node_index] = temp;
+          parent_node_index = child_node_index;
+        } else break;
+      } else {
+        if (this._tree[parent_node_index] > this._tree[child_node_index]) {
           const temp = this._tree[parent_node_index];
           this._tree[parent_node_index] = this._tree[child_node_index];
           this._tree[child_node_index] = temp;
           parent_node_index = child_node_index;
         } else break;
       }
-    }else {
-      while (true) {
-        const child_node_index = this._tree[parent_node_index*2] < this._tree[parent_node_index*2+1] ? parent_node_index*2 : parent_node_index*2+1;        
-        if ((child_node_index < this._tree.length) && (this._tree[parent_node_index] > this._tree[child_node_index])) {
-          const temp = this._tree[parent_node_index];
-          this._tree[parent_node_index] = this._tree[child_node_index];
-          this._tree[child_node_index] = temp;
-          parent_node_index = child_node_index;
-        } else break;
-      }
+      
     }
+    return return_value;
+  }
+
+  sort() {
+    const array = new Array();
+    while (this._tree.length > 1) {
+      array.push(this.pop());
+    }
+    this._tree.push(...array);
   }
 }
 
