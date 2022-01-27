5365b25a82b68537c4407e5463fa2193ae75ea66
park singyu <psk876@naver.com>
2022-01-27 19:34:41 +0900

    change swap array value method

--- function/sort/index.ts
+++ function/sort/index.ts
@@ -1,11 +1,7 @@
 function bubbleSort(array: number[]) {
   for (let i = 1; i < array.length; i++) {
     for (let j = 0; j < array.length-i; j++) {
-      if (array[j] > array[j+1]) {
-        const temp = array[j];
-        array[j] = array[j+1];
-        array[j+1] = temp;
-      }
+      if (array[j] > array[j+1]) swap(array,j,j+1);
     }
   }
 }
@@ -13,11 +9,7 @@ function bubbleSort(array: number[]) {
 function selectionSort(array: number[]) {
   for (let i = 0; i < array.length; i++) {
     for (let j = i+1; j < array.length; j++) {
-      if (array[i] > array[j]) {
-        const temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
-      }
+      if (array[i] > array[j]) swap(array,i,j);
     }
   }
 }
@@ -27,9 +19,7 @@ function insertSort(array: number[]) {
     z = i;
     for (let j = i-1; j >= 0; j--) {
       if (array[z] > array[j]) break;
-      const temp = array[z];
-      array[z] = array[j];
-      array[j] = temp;
+      swap(array,j,z);
       z = j;
     }
   }
